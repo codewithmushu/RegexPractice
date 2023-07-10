d3e840d0c5ddd356c71ee1d1d9f32d90ae59bf3c
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-07-10 08:53:02 +0530

    [Muskan] Add : Matching Name string

---
+++ RegexDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.WriteLine("Please choose the option below:");
+                Console.WriteLine("1: Regex exp");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Regex_Match reger_match = new Regex_Match();
+                        reger_match.MyRegex();
+                        break;
+
+                }
+            }
+        }
+    }
+
+}
---
+++ RegexDemo/Regex_Match.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexDemo
+{
+    //A regex (Regular Expresion) is a pattern that is used to check wheather a given string matches that.
+    public class Regex_Match
+    {
+        static string pattern = "^cen$";
+        //^ it will indicate string start
+        // $ it will indicate the end of string
+
+        public void MyRegex()
+        {
+            //we have created object of class
+            //we have pass a parameter pattern in given class constructor
+            Regex regex = new Regex(pattern);
+            //is match return 
+            if (regex.IsMatch("Muskan"))
+            {
+                Console.WriteLine("String is matching with Pattern");
+            }
+            else
+            {
+                Console.WriteLine("String doesnt matches");
+            }
+        }
+
+
+        public void CheckName(string name)
+        {
+            string myName = "^[A-Z][a-z]{3,}?";
+            if (Regex.IsMatch(name, myName))
+            {
+                Console.WriteLine("String is matching with Pattern");
+            }
+            else
+            {
+                Console.WriteLine("String doesnt matches");
+            }
+
+        }
+    }
+}
